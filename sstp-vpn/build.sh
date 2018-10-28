68598a82f0194040ca1b0822a270fca87a98b5ff
Dambaev Alexander <ice.redmine@gmail.com>
2018-10-28 16:35:40 +0800

    Revert "debug"
    This reverts commit 5ba49f0e7fb69424d7a354ca7cb71272f0a0afbf.

--- sstp-vpn/build.sh
+++ sstp-vpn/build.sh
@@ -15,7 +15,7 @@ dpkg -l > clean_versions
 
 apt install -y pkg-config dh-make build-essential libevent-dev libssl-dev ppp-dev autotools-dev
 cd /usr/src/app/sstp-client-$SSTP_VERSION
-dh_make --createorig -s -y
+LOGNAME=build USER=build dh_make --createorig -s -y
 # we need DEB_BUILD_OPTIONS=nocheck because build environment will not allow
 # us to modify routing table
 DEB_BUILD_OPTIONS=nocheck dpkg-buildpackage -us -uc
